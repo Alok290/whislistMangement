42f3da6790df650aebb44008145c091a4880b161
Alok290 <79790013+Alok290@users.noreply.github.com>
2024-02-19 13:44:46 +0530

    final

--- src/main/java/com/example/whislistMangement/Service/CustomUserDetailService.java
+++ src/main/java/com/example/whislistMangement/Service/CustomUserDetailService.java
@@ -9,9 +9,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class CustomUserDetailService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
